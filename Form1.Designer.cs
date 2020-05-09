ab24acce95fd7724c43ee2ee562f08e7d7e627ec
sumczan <45795394+sumczan@users.noreply.github.com>
2020-05-09 18:02:38 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace $safeprojectname$
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(804, 17);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Selected += new System.Windows.Forms.TabControlEventHandler(this.tabControl1_Selected);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(796, 0);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(667, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New Tab";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(667, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Remove Tab";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Pen",
+            "Rectangle",
+            "Line"});
+            this.comboBox1.Location = new System.Drawing.Point(667, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Black",
+            "Red",
+            "Green",
+            "Blue",
+            "Yellow"});
+            this.comboBox2.Location = new System.Drawing.Point(667, 97);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "2",
+            "4",
+            "6",
+            "8",
+            "10"});
+            this.comboBox3.Location = new System.Drawing.Point(667, 125);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(656, 477);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(693, 453);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Minimize";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(667, 199);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(667, 228);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(693, 424);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Maximize";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(804, 488);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.tabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace $safeprojectname$
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private enum Shape
+        {
+            Pen,
+            Rectangle,
+            Line
+        }
+
+        private Shape currentShape;
+        private Color currentColor;
+
+        Point start;
+        Point end;
+
+        List<Bitmap> bitmap = new List<Bitmap>();
+        Graphics drawing;
+
+        Pen pen;
+
+        MyShape myShape;
+
+        float penThickness;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+
+            tabControl1.SelectedIndex = 0;
+            bitmap.Add(new Bitmap(pictureBox1.Width, pictureBox1.Height));
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Add(new TabPage());
+            bitmap.Add(new Bitmap(pictureBox1.Width, pictureBox1.Height));
+            tabControl1.SelectedIndex = tabControl1.TabCount - 1;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int current = tabControl1.SelectedIndex;
+            tabControl1.TabPages.RemoveAt(current);
+            bitmap.RemoveAt(current);
+            if (tabControl1.SelectedIndex == -1)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+            tabControl1.SelectedIndex = ((current != 0) ? current - 1 : current);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            bitmap[tabControl1.SelectedIndex].Save(saveFileDialog1.FileName + ".png", ImageFormat.Png);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files|*.png;";
+            openFileDialog1.ShowDialog();
+            bitmap[tabControl1.SelectedIndex] = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bitmap[tabControl1.SelectedIndex];
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            currentShape = (Shape)Enum.Parse(typeof(Shape), (String)comboBox1.SelectedItem);
+            currentColor = Color.FromName((String)comboBox2.SelectedItem);
+            penThickness = int.Parse(comboBox3.SelectedItem.ToString());
+
+            Brush brush = new SolidBrush(currentColor);
+            pen = new Pen(brush);
+            pen.Width = penThickness;
+
+            start.X = e.X;
+            start.Y = e.Y;
+
+            switch (currentShape)
+            {
+                case Shape.Pen:
+                    myShape = new MyShape();
+                    break;
+                case Shape.Rectangle:
+                    myShape = new MyRectangle();
+                    break;
+                case Shape.Line:
+                    myShape = new MyLine();
+                    break;
+                default:
+                    break;
+            }
+            myShape.start = start;
+
+            drawing = pictureBox1.CreateGraphics();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                end.X = e.X;
+                end.Y = e.Y;
+                myShape.end = end;
+                bitmap[tabControl1.SelectedIndex] = myShape.Draw(pen, drawing, bitmap[tabControl1.SelectedIndex], true);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            bitmap[tabControl1.SelectedIndex] = myShape.Draw(pen, drawing, bitmap[tabControl1.SelectedIndex], false);
+        }
+
+        private void tabControl1_Selected(object sender, TabControlEventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = bitmap[tabControl1.SelectedIndex];
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            // po dodaniu poruszania oknem przestało działać;
+            this.WindowState = ((!(this.WindowState == FormWindowState.Maximized)) ? FormWindowState.Maximized : FormWindowState.Normal);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+                button6.Text = "Normalize";
+            }
+            else if(this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                button6.Text = "Maximize";
+            }
+        }
+    }
+
+    public class MyShape
+    {
+        public Point start { get; set; }
+        public Point end { get; set; }
+
+        public virtual Bitmap Draw(Pen pen, Graphics drawing, Bitmap bitmap, bool clean)
+        {
+            Graphics draw = Graphics.FromImage(bitmap);
+
+            drawing.SmoothingMode = SmoothingMode.AntiAlias;
+            draw.SmoothingMode = SmoothingMode.AntiAlias;
+
+            drawing.DrawLine(pen, this.start, this.end);
+            draw.DrawLine(pen, this.start, this.end);
+
+            this.start = this.end;
+            return bitmap;
+        }
+    }
+
+    public class MyRectangle : MyShape
+    {
+        public override Bitmap Draw(Pen pen, Graphics drawing, Bitmap bitmap, bool clean)
+        {
+            int x;
+            int y;
+            int width;
+            int height;
+            Graphics draw = Graphics.FromImage(bitmap);
+
+            x = ((start.X < end.X) ? start.X : end.X);
+            y = ((start.Y < end.Y) ? start.Y : end.Y);
+            width = Math.Abs(start.X - end.X);
+            height = Math.Abs(start.Y - end.Y);
+
+            if (!clean)
+            {
+                drawing.SmoothingMode = SmoothingMode.AntiAlias;
+                draw.SmoothingMode = SmoothingMode.AntiAlias;
+
+                drawing.DrawRectangle(pen, x, y, width, height);
+                draw.DrawRectangle(pen, x, y, width, height);
+            }
+
+            return bitmap;
+        }
+    }
+
+    public class MyLine : MyShape
+    {
+        public override Bitmap Draw(Pen pen, Graphics drawing, Bitmap bitmap, bool clean)
+        {
+            Graphics draw = Graphics.FromImage(bitmap);
+            drawing.SmoothingMode = SmoothingMode.AntiAlias;
+            draw.SmoothingMode = SmoothingMode.AntiAlias;
+            if (!clean)
+            {
+                drawing.DrawLine(pen, this.start, this.end);
+                draw.DrawLine(pen, this.start, this.end);
+            }
+            return bitmap;
+        }
+    }
+}
